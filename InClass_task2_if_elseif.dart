c3f2dce479db6e13e2f126ce7d8a89d5e16aebc1
Muhammd-Shehzad <127086042+Muhammd-Shehzad@users.noreply.github.com>
2024-04-18 00:38:04 +0500

    Add files via upload

---
+++ InClass_task2_if_elseif.dart
@@ -0,0 +1,19 @@
+void main () {
+
+  int main_value = 5;
+  int user_input = 13;
+
+  if (main_value == user_input)
+  {
+    print ('main_value and user_input are equal');
+
+  } else if (main_value > user_input)
+  {
+    print ("main_value is greater than user_input");
+
+  } else if (main_value< user_input)
+  {
+    print ('main_value is less than user_input');
+
+  }
+}
